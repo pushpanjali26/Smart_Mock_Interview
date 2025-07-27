3bc35332ad3aba1cd19679259cdff6ccc9be5018
pushpanjali26 <158044330+pushpanjali26@users.noreply.github.com>
2025-07-27 11:06:44 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}', // Include files in the `app` directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
